0c00ea61c940d3a82eaf42f2c4873acd9dc89db5
HaDat <dat.ha@ascendcorp.com>
2018-10-30 19:46:32 +0700

    delete redandunt folder

--- comic-backend/src/main/java/com/comic/backend/constant/SecurityConstant.java
+++
@@ -1,6 +0,0 @@
-package com.comic.backend.constant;
-
-public class SecurityConstant {
-    public static final Integer EXPIRE_MINUTES = 5;
-    public static final String SECRET_KEY = "comic@123";
-}
