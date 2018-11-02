cb610b8d66e884eb67d6e89d094f12829bd6db8e
HaDat <dat.ha@ascendcorp.com>
2018-11-02 15:06:18 +0700

    Remember me function

--- comic-frontend/src/app/+auth/+login/login.component.ts
+++ comic-frontend/src/app/+auth/+login/login.component.ts
@@ -4,22 +4,32 @@ import {AuthService} from "../../core/_auth/auth.service";
 import {environment} from "../../../environments/environment";
 import {BaseComponent} from "../base/BaseComponent";
 
+const USER_NAME_REMEMBER_KEY='username_remember';
+const PASSWORD_REMEMBER_KEY='password_remember';
+
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html'
 })
+
 export class LoginComponent extends BaseComponent implements OnInit {
 
     constructor(private router: Router, private as: AuthService) {
         super();
     }
 
+
     password: string;
     username: string;
+    rememberMe: boolean = true;
 
     ngOnInit() {
         if (this.as.isUserAuthenticated()) {
             window.location.href = environment.authen.home_url;
+        } else {
+            this.username = localStorage.getItem(USER_NAME_REMEMBER_KEY);
+            this.password = localStorage.getItem(PASSWORD_REMEMBER_KEY);
+            console.log('remember me: ' + this.rememberMe);
         }
     }
 
@@ -30,6 +40,7 @@ export class LoginComponent extends BaseComponent implements OnInit {
                 var loginResult = response.success;
                 if (loginResult == true) {
                     this.as.loginSuccess(response);
+                    this.rememberLogin();
                 } else {
                     this.errorHandle(response.exceptionMessage);
                 }
@@ -37,6 +48,17 @@ export class LoginComponent extends BaseComponent implements OnInit {
             error => {
                 this.errorHandle("Has error!");
             })
+    }
+
+    rememberLogin() {
+        if (this.rememberMe) {
+            console.log('remember me');
+            localStorage.setItem(USER_NAME_REMEMBER_KEY, this.username);
+            localStorage.setItem(PASSWORD_REMEMBER_KEY, this.password);
+        } else {
+            localStorage.removeItem(USER_NAME_REMEMBER_KEY);
+            localStorage.removeItem(PASSWORD_REMEMBER_KEY);
+        }
     }
 
 }
