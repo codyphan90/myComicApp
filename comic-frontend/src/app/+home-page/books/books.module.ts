cd8e8ef4f4e684a68f8c51f1c58a6d8b3d435f35
Phan Viet Anh <phanvietanh90@gmail.com>
2018-11-09 18:58:02 +0700

    create list book module

--- comic-frontend/src/app/+home-page/books/books.module.ts
+++ comic-frontend/src/app/+home-page/books/books.module.ts
@@ -6,14 +6,16 @@ import {NgModule} from '@angular/core';
 
 import {BooksRoutingModule} from './books-routing.module';
 import {BooksComponent} from './books.component';
+import {ListBookComponent} from '../../base/list.book.component'
 import {UserFormModule} from "../../base/user.form.module";
 import {UserService} from "../../+service/user.service";
 import {UserGroupService} from "../../+service/user.group.service";
+import { ListBookModule } from 'app/base/list.book.module';
 
 
 @NgModule({
     imports: [
-        BooksRoutingModule, UserFormModule
+        BooksRoutingModule, UserFormModule, ListBookModule
     ],
     declarations: [
         BooksComponent
