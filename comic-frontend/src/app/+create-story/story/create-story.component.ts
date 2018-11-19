fd6ebf7323a14e741eb73d025ffd65b478e1d4e4
HaDat <dat.ha@ascendcorp.com>
2018-11-19 19:57:55 +0700

    create book

--- comic-frontend/src/app/+create-story/story/create-story.component.ts
+++ comic-frontend/src/app/+create-story/story/create-story.component.ts
@@ -9,14 +9,71 @@ import {BaseComponent} from "../../base/base.component";
     styleUrls: ['./create-story.component.css']
 })
 export class CreateStoryComponent extends BaseComponent implements OnInit {
-    // listType: any = {
-    //     MINE: {code:"MINE", des:"My books"}, FRIEND: {code:"FRIEND", des:"My friend's books"}, TRENDING: {code:"TRENDING", des:"Trending books"}
-    // };
-    //
-    // LIST_TYPE = this.listType;
     constructor() {super(); }
 
+    demo2: any = [
+        {"content": "<span><i class=\"fa fa-lg fa-book\"></i>Parent/span>", "expanded": true, "children": [
+                {"content": "<span> Administrators</span>", "expanded": true, "children": [
+                        {"content": "<span> <label class=\"checkbox inline-block\"><input type=\"checkbox\" name=\"checkbox-inline\"><i></i>Michael.Jackson</label> </span>"},
+                        {"content": "<span> <label class=\"checkbox inline-block\"><input type=\"checkbox\" checked=\"checked\" name=\"checkbox-inline\"><i></i>Sunny.Ahmed</label> </span>"},
+                        {"content": "<span> <label class=\"checkbox inline-block\"><input type=\"checkbox\" checked=\"checked\" name=\"checkbox-inline\"><i></i>Jackie.Chan</label> </span>"}
+                    ]},
+                {"content": "<span> <label class=\"checkbox inline-block\"><input type=\"checkbox\" name=\"checkbox-inline\"><i></i>Michael.Jackson</label> </span>"},
+                {"content": "<span> <label class=\"checkbox inline-block\"><input type=\"checkbox\" checked=\"checked\" name=\"checkbox-inline\"><i></i>Sunny.Ahmed</label> </span>"},
+                {"content": "<span> <label class=\"checkbox inline-block\"><input type=\"checkbox\" checked=\"checked\" name=\"checkbox-inline\"><i></i>Jackie.Chan</label> </span>"},
+                {"content": "<span> Child</span>", "expanded": true, "children": [
+                        {"content": "<span><i class=\"icon-leaf\"></i> Grand Child</span>"},
+                        {"content": "<span><i class=\"icon-leaf\"></i> Grand Child</span>"},
+                        {"content": "<span> Grand Child</span>",  "children": [
+                                {"content": "<span> Great Grand Child</span>", "children": [
+                                        {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"},
+                                        {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"}
+                                    ]},
+                                {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"},
+                                {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"},
+                                {"content": "<span><i class=\"icon-leaf\"></i> Great Grand Child</span>"},
+                                {"content": "<span><i class=\"icon-leaf\"></i> Great Grand Child</span>"}
+                            ]},
+                        {"content": "<span> Great Grand Child</span>", "children": [
+                                {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"},
+                                {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"}
+                            ]},
+                        {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"},
+                        {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"},
+                        {"content": "<span><i class=\"icon-leaf\"></i> Great Grand Child</span>"},
+                        {"content": "<span><i class=\"icon-leaf\"></i> Great Grand Child</span>"}
+                    ]},
+                {"content": "<span><i class=\"icon-leaf\"></i> Grand Child</span>"},
+                {"content": "<span><i class=\"icon-leaf\"></i> Grand Child</span>"},
+                {"content": "<span> Grand Child</span>", "children": [
+                        {"content": "<span> Great Grand Child</span>", "children": [
+                                {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"},
+                                {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"}
+                            ]},
+                        {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"},
+                        {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"},
+                        {"content": "<span><i class=\"icon-leaf\"></i> Great Grand Child</span>"},
+                        {"content": "<span><i class=\"icon-leaf\"></i> Great Grand Child</span>"}
+                    ]},
+                {"content": "<span> Great Grand Child</span>", "children": [
+                        {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"},
+                        {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"}
+                    ]},
+                {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"},
+                {"content": "<span><i class=\"icon-leaf\"></i> Great great Grand Child</span>"},
+                {"content": "<span><i class=\"icon-leaf\"></i> Great Grand Child</span>"},
+                {"content": "<span><i class=\"icon-leaf\"></i> Great Grand Child</span>"}
+            ]},
+        {"content": "<span><i class=\"fa fa-lg fa-folder-open\"></i> Parent2</span>", "children": [
+                {"content": "<span><i class=\"icon-leaf\"></i> Child</span>"}
+            ]}
+    ];
+
     ngOnInit() {
+
     }
 
+    changeListener(payload) {
+        console.log('change payload', payload)
+    }
 }
--- comic-frontend/src/app/+create-story/story/create-story.module.ts
+++ comic-frontend/src/app/+create-story/story/create-story.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CreateStoryRoutingModule} from './create-story-routing.module';
 import {CreateStoryComponent} from './create-story.component';
+import {TreeViewModule} from "../../shared/ui/tree-view/tree-view.module";
+import {SmartadminWidgetsModule} from "../../shared/widgets/smartadmin-widgets.module";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
     imports: [
-        CreateStoryRoutingModule
+        CreateStoryRoutingModule, TreeViewModule, SmartadminWidgetsModule, CommonModule,
+        FormsModule,
     ],
     declarations: [
         CreateStoryComponent
